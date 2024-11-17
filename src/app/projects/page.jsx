c06d34e0701b03c1c1a8b3865f1681f441bbadb9
Vaibhav262610 <branda56235@gmail.com>
2024-11-17 20:55:36 +0530

    hello

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex justify-center items-center h-screen w-full'>
-      ABOUT SECTION
+      THIS IS ABOUT SECTION
     </div>
   )
 }
