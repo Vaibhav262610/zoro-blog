5c03d2276db8db4f4031b1ade351770ffd4155fe
Vaibhav262610 <branda56235@gmail.com>
2024-11-19 01:17:48 +0530

    h

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      THIS IS HEADER SECTION
+      THIS IS HEADER SECTION      
     </div>
   );
 }
