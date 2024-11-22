53db0e0d8f986de66214941aec4605d0d46822de
Vaibhav262610 <branda56235@gmail.com>
2024-11-22 12:23:24 +0530

    "h"

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,42 +1,45 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-    
-        clerkId:{
-            type: String,
-            required : true,
-            unique : true,
-        },
-        
-        email:{
-            type: String,
-            required : true,
-            unique : true,
-        },
-        firstName:{
-            type: String,
-            required : true,
-        },
-        lastName:{
-            type: String,
-            required : true,
-        },
-        username:{
-            type: String,
-            required : true,
-            unique:true,
-        },
-        profilePicture:{
-            type:String,
-            required : true,
-        },
-        isAdmin:{
-            type:Boolean,
-            default: false,
-        },
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-    },{timestamps:true}
-);     
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-const User = mongoose.models.User || mongoose.model('User',userSchema);
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    profilePicture: {
+      type: String,
+      required: false,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 export default User;
